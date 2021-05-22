6edb27837d5259c15ab14ddf8a00186166a4b3bf
Hrishikesh Sriperumbudur <71116245+xenonstride@users.noreply.github.com>
2021-05-22 13:21:35 +0530

    add comments

--- spotify-yt-mp3.py
+++ spotify-yt-mp3.py
@@ -11,42 +11,50 @@ import eyed3
 from apiclient.discovery import build
 import shutil
 
-with open("my_secret.json", 'r') as f:
+#GET API KEYS FROM secret.json file
+with open("secret.json", 'r') as f:
     data = json.load(f)
     SP_CLIENT_ID = data["client_id"]
     SP_CLIENT_SECRET = data["client_secret"]
 
+#SPOTIFY API AUTHENTICATION
 AUTH_URL = 'https://accounts.spotify.com/api/token'
-
 auth_response = requests.post(AUTH_URL, {
     'grant_type': 'client_credentials',
     'client_id': SP_CLIENT_ID,
     'client_secret': SP_CLIENT_SECRET}
 )
-
 auth_response_data = auth_response.json()
 access_token = auth_response_data['access_token']
-
 headers = {
     'Authorization': f'Bearer {access_token}'
 }
 
+#INVALID CHARACTERS FOR FILE NAMES
 invalid_chars=['\\','/',':','*','?','"','<','>','|']
 
+#DOWNLOADER FUNCTION FOR MULTITHREADING
 def downloader(link, track_number, requested_tracks, requested_tracks_artists, album_name):
+
+    #FILE NAME
     new_title = requested_tracks[track_number] + f' [{album_name}]'
     for i in invalid_chars:
         if i in new_title:
             new_title=new_title.replace(i,'')
 
+    #DOWNLOAD FILE
     YouTube(link).streams.first().download(filename=new_title)
-    # CONVERT
+    
+    # CONVERT MP4 TO MP3
     video = VideoFileClip(os.getcwd()+'\\'+new_title+'.mp4')
     audio = video.audio
     audio.write_audiofile(os.getcwd()+'\\'+new_title+'.mp3', logger=None)
     audio.close()
     video.close()
+    
+    #DELETE MP4 FILE
     os.remove(os.getcwd()+'\\'+new_title+'.mp4')
+
     # ADD METADATA
     mp3 = eyed3.load(os.getcwd()+'\\'+new_title+'.mp3')
     if (mp3.tag == None):
@@ -59,35 +67,37 @@ def downloader(link, track_number, requested_tracks, requested_tracks_artists, a
 
 
 def main(album_name):
-    prev_dir = os.getcwd()
-    # replace with your music directory
+
+    # REPLACE WITH YOUR MUSIC DIRECTORY
     os.chdir(r"C:\Users\hsrip\Music\My-Music")
 
-    # download album art
+    # DOWNLOAD ALBUM ART
     if path.exists('art.png'):
         os.remove('art.png')
 
-    # search spotify
+    # SEARCH SPOTIFY FOR GIVEN ALBUM
     Sp_BASE_URL = 'https://api.spotify.com/v1/'
     album_name_api_req = album_name.replace(" ", "%20")
     query1 = 'search?q=album:'+album_name_api_req+'&type=album'
     spotify_a1 = requests.get(Sp_BASE_URL+query1, headers=headers)
     spotify_a2 = spotify_a1.json()
 
+    #GET ALBUM ID IF FOUND
     try:
         album_id = spotify_a2['albums']['items'][0]['id']
+    #IF ALBUM NOT FOUND
     except IndexError:
         print("Album not found on Spotify")
         return
 
-    # get tracks from top search result
+    # GET TRACKS OF ALBUM FROM TOP SEARCH RESULT IN SPOTIFY
     query2 = 'albums/'+album_id
     spotify_b1 = requests.get(Sp_BASE_URL+query2, headers=headers)
     spotify_b2 = spotify_b1.json()
 
     track_list = []
     no_of_tracks = int(spotify_b2['total_tracks'])
-    print("Track list : ")
+    print("Tracks list : ")
     for i in range(no_of_tracks):
         track_list.append(spotify_b2['tracks']['items'][i]['name'])
         print(f"{i+1}) "+track_list[i])
@@ -99,10 +109,14 @@ def main(album_name):
     print("-->> ", end='')
     requested_str = str(input())
     requested_tracks_index = []
+
+    #IF WRONG ALBUM FOUND
     if requested_str == 'no':
         return
+    #DOWNLOAD ALL SONGS
     elif requested_str == '':
         requested_tracks_index = [i for i in range(no_of_tracks)]
+    #DOWNLOAD ONLY REQUESTED SONGS
     else:
         requested_tracks_index = requested_str.split(',')
         requested_tracks_index = [int(a)-1 for a in requested_tracks_index]
@@ -124,6 +138,7 @@ def main(album_name):
     with open("art.png", 'wb') as f:
         f.write(r.content)
     
+    #CREATE ALBUM ART COPIES FOR EACH THREAD
     os.rename("art.png","art0.png")
     for i in range(len(requested_tracks)-1):
         shutil.copyfile("art0.png",f"art{i+1}.png")
@@ -176,13 +191,15 @@ def main(album_name):
     threads=[]
     
     for i in range(len(requested_tracks_index)):
-        # DOWNLOAD
+        # CREATE THREADS
         threadObj=threading.Thread(target=downloader,args=[video_links[i],i,requested_tracks,requested_tracks_artists,album_name])
         threads.append(threadObj)
     
+    #STARTS THREADS
     [t.start() for t in threads]
     [t.join() for t in threads]
 
+    #DELETE ALBUM ART FILES
     for i in range(len(requested_tracks)):
         os.remove(f"art{i}.png")
 
